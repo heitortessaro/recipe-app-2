6d616df6a2bd8e84bdd885e5d29f6c35b8568c9a
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-14 17:23:21 -0300

    chore: add not found page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Login from './pages/Login.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 function App() {
   return (
     <div className="h-screen">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
