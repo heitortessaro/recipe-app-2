a6613151f1d73522bc9857d933aa447eba908b94
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-14 15:40:20 -0300

    chore: install and configure flowbite and react-flowbite.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {Counter} from './features/counter/Counter';
 import './App.css';
+import { Card } from 'flowbite-react';
 
 function App() {
   return (
     <div className="App">
-      <p className='text-ll'>Teste</p>
+      <p className="text-ll">Teste</p>
+      <Card href="#">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </Card>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
