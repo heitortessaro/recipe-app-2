a877d6218310bc4baa3ce1ee7974d28796a7fd84
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-14 16:11:09 -0300

    feat: create Login page.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import {Counter} from './features/counter/Counter';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import { Card } from 'flowbite-react';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <p className="text-ll">Teste</p>
-      <Card href="#">
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-      </Card>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/game" element={<Game />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/ranking" element={<Ranking />} /> */}
+
+        {/* <Route  path="/" component={ Login } />
+        <Route  path="/foods" component={ Foods } />
+        <Route  path="/drinks" component={ Drinks } />
+        
+        <Route  path="/foods/:id" component={ Recepie } />
+        <Route  path="/drinks/:id" component={ Recepie } />
+        
+        <Route  path="/foods/:id/in-progress" component={ RecepieInProgres } />
+        <Route  path="/drinks/:id/in-progress" component={ RecepieInProgres } />
+        
+        <Route  path="/explore" component={ Explore } />
+        <Route  path="/explore/foods" component={ ExploreFoods } />
+        <Route  path="/explore/drinks" component={ ExploreDrinks } />
+        
+        <Route  path="/explore/foods/ingredients" component={ ExploreIngredients } />
+        <Route 
+          path="/explore/drinks/ingredients"
+          component={ ExploreIngredients }
+        />
+        <Route
+          
+          path="/explore/foods/nationalities"
+          component={ ExploreNacionalities }
+        />
+        
+        <Route  path="/profile" component={ Profile } />
+        <Route  path="/done-recipes" component={ DoneRecipes } />
+        <Route
+          
+          path="/favorite-recipes"
+          component={ FavoriteRecipies }
+        />
+        <Route path="/explore" component={ NotFound } /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+// import EmailValidator from 'email-validator';
+// import Input from '../components/Input';
+
+export default function Login() {
+  // const [login, setLogin] = useState({
+  //   email: '',
+  //   password: '',
+  //   disabledButton: true,
+  // });
+
+  // const { email, password, disabledButton } = login;
+
+
+  // const validLogin = () => {
+  //   const numberValid = 6;
+  //   const validEmail = EmailValidator.validate(email); // true ou false
+  //   const valueToInput = password.length > numberValid;
+
+  //   if (valueToInput && validEmail) {
+  //     setLogin({ ...login, disabledButton: false });
+  //   } else {
+  //     setLogin({ ...login, disabledButton: true });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   validLogin();
+  // }, [email, password]);
+
+  // const handleChangeLogin = ({ target: { name, value } }) => {
+  //   setLogin({ ...login, [name]: value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const mealsToken = 1;
+  //   const cocktailsToken = 1;
+  //   localStorage.setItem('mealsToken', JSON.stringify(mealsToken));
+  //   localStorage.setItem('cocktailsToken', JSON.stringify(cocktailsToken));
+  //   localStorage.setItem(
+  //     'user',
+  //     JSON.stringify({ email }),
+  //   );
+  //   history.push('/foods');
+  // };
+
+  return (
+    <form>
+      <div class="mb-6">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+        <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required="" />
+      </div>
+      <div class="mb-6">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+      </div>
+      <div class="flex items-start mb-6">
+        <div class="flex items-center h-5">
+          <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
+        </div>
+        <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+      </div>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+
+    // <section className="containerLogin">
+    //   <div className="contextBoxLogin">
+
+    //     <h1>Login</h1>
+
+    //     <form onSubmit={ handleSubmit }>
+    //       <Input
+    //         dataTestid="email-input"
+    //         label="Email: "
+    //         type="text"
+    //         onChange={ handleChangeLogin }
+    //         value={ email }
+    //         name="email"
+    //         id="emaildId"
+    //         placeholder="Digite seu email"
+    //         required
+    //       />
+
+    //       <Input
+    //         dataTestid="password-input"
+    //         label="Senha: "
+    //         type="password"
+    //         onChange={ handleChangeLogin }
+    //         value={ password }
+    //         name="password"
+    //         id="passwordId"
+    //         placeholder="Digite sua senha"
+    //         required
+    //       />
+
+    //       <button
+    //         type="submit"
+    //         data-testid="login-submit-btn"
+    //         disabled={ disabledButton }
+    //       >
+    //         Entrar
+    //       </button>
+    //     </form>
+    //   </div>
+    // </section>
+  );
+}
