6fad344e28391a5cf5f72b4dd5bbbaca53e24ed8
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-14 16:38:47 -0300

    fix: Fix router use. Add BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/game" element={<Game />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/ranking" element={<Ranking />} /> */}
-
-        {/* <Route  path="/" component={ Login } />
-        <Route  path="/foods" component={ Foods } />
-        <Route  path="/drinks" component={ Drinks } />
-        
-        <Route  path="/foods/:id" component={ Recepie } />
-        <Route  path="/drinks/:id" component={ Recepie } />
-        
-        <Route  path="/foods/:id/in-progress" component={ RecepieInProgres } />
-        <Route  path="/drinks/:id/in-progress" component={ RecepieInProgres } />
-        
-        <Route  path="/explore" component={ Explore } />
-        <Route  path="/explore/foods" component={ ExploreFoods } />
-        <Route  path="/explore/drinks" component={ ExploreDrinks } />
-        
-        <Route  path="/explore/foods/ingredients" component={ ExploreIngredients } />
-        <Route 
-          path="/explore/drinks/ingredients"
-          component={ ExploreIngredients }
-        />
-        <Route
-          
-          path="/explore/foods/nationalities"
-          component={ ExploreNacionalities }
-        />
-        
-        <Route  path="/profile" component={ Profile } />
-        <Route  path="/done-recipes" component={ DoneRecipes } />
-        <Route
-          
-          path="/favorite-recipes"
-          component={ FavoriteRecipies }
-        />
-        <Route path="/explore" component={ NotFound } /> */}
       </Routes>
     </div>
   );
