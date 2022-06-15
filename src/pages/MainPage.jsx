978b4932351038f4bab7a6ef6d7233c988561a6f
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-15 16:43:11 -0300

    chore: add page title dynamically.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function MainPage() {
+  const location = useLocation();
+  const title = location.pathname.includes('foods')? 'Foods' : 'Drinks';
   return (
     <div className=''>
-      <Header enableSearch={true} title={'Drinks'}/>
+      <Header enableSearch={true} title={title}/>
       {/* <SearchBar /> 
       <Alert /> */}
       <div>
