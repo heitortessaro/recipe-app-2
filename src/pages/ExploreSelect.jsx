aa1243436de6870d849f5c5fc7fa1a2467c04a53
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-22 18:32:42 -0300

    chore: Change title of pages explore and exploreSelect.

--- src/pages/ExploreSelect.jsx
+++ src/pages/ExploreSelect.jsx
@@ -4,7 +4,6 @@ import BtnShelf from '../components/BtnShelf';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 import OverlayImageBtn from '../components/OverlayImageBtn';
-import SearchBar from '../components/SearchBar';
 
 const FOOD_INGREDIENT = 'https://images.unsplash.com/photo-1470549813517-2fa741d25c92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW5ncmVkaWVudHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60';
 const FOOD_COUNTRY = 'https://images.unsplash.com/photo-1521295121783-8a321d551ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmF0aW9ucyUyMG1hcHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60';
@@ -15,14 +14,13 @@ const DRINK_SURPRISE = 'https://images.unsplash.com/photo-1438557068880-c5f47483
 
 export default function ExploreSelect() {
   const location = useLocation();
-  const title = location.pathname.includes('foods') ? 'foods' : 'drinks';
-  const suprise_me = title === 'foods' ? FOOD_SURPRISE : DRINK_SURPRISE;
-  const ingredient = title === 'foods' ? FOOD_INGREDIENT : DRINK_INGREDIENT;
-  const showCountry = title === 'foods' ? true : false;
+  const title = location.pathname.includes('foods') ? 'Foods' : 'Drinks';
+  const suprise_me = title === 'Foods' ? FOOD_SURPRISE : DRINK_SURPRISE;
+  const ingredient = title === 'Foods' ? FOOD_INGREDIENT : DRINK_INGREDIENT;
+  const showCountry = title === 'Foods' ? true : false;
   return (
     <div className='flex flex-col min-h-full'>
-      <Header enableSearch={false} title="Explore" />
-      <SearchBar />
+      <Header enableSearch={false} title={`Explore ${title}`} />
       <BtnShelf>
         <OverlayImageBtn img={ingredient} destiny={`${location.pathname}/ingredients`} name='Explore By Ingredients' />
         <OverlayImageBtn img={suprise_me} destiny={`${location.pathname}/surprise`} name='Surprise Me' />
