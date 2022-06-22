4d9d6a62120b1257dd03206c03ca00a6944531a4
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-22 14:28:12 -0300

    chore: Add hover scale transition to cards.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -6,12 +6,12 @@ export default function RecipeCard() {
   const navigate = useNavigate();
   return(
     <div 
-      class="card m-2 w-60 bg-base-100 shadow-xl cursor-pointer "
+      className="card m-2 w-60 bg-base-100 shadow-xl cursor-pointer transform transition duration-200 hover:scale-110"
       onClick={() => navigate('/teste')}
     >
       <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-      <div class="card-body p-3 w-full">
-        <p class="text-center text-xl font-semibold capitalize">Title! nove</p>
+      <div className="card-body p-3 w-full">
+        <p className="text-center text-xl font-semibold capitalize">Title! nove</p>
       </div>
     </div>
   )
