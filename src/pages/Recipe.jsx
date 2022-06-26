09ce07fe368f47f2702de551f925f870d6b2984b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-26 08:59:04 -0300

    chore: add video to the recipe component.

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
 import IngredientsList from '../components/IngredientsList';
 
 
@@ -12,9 +12,16 @@ export default function Recipe() {
       <div>imgagem comida</div>
       <div>nome + botões</div>
       <IngredientsList />
-      <div>description</div> 
+      <div>description</div>
       <div> youtube video </div>
-      <Carousel />
+      <div className="bg-base-300 grow py-2">
+        <div className="w-5/6 sm:w-3/4 md:max-w-screen-lg mx-auto">
+          <div className="aspect-w-16 aspect-h-9">
+            <iframe src="https://www.youtube.com/embed/r9jwGansp1E" allowFullScreen />
+          </div>
+        </div>
+      </div>
+      {/* <Carousel /> */}
       <div>start button</div>
     </div>
   );
