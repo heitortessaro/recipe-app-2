32e09d5817fe02550e709aa741daa1e01f82033b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-26 09:23:39 -0300

    chore: remove unused code from FooterStartButton.

--- src/components/FooterStartRecipe.jsx
+++ src/components/FooterStartRecipe.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import mealIcon from '../images/mealIcon.svg';
-import exploreIcon from '../images/exploreIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
 
 export default function FooterStartRecipe() {
   const navigate = useNavigate();
@@ -10,7 +7,7 @@ export default function FooterStartRecipe() {
     <div className="fixed bottom-0 w-full bg-orange-200 hover:bg-orange-300 flex justify-around">
         <button
           onClick={() => navigate('/foods')}
-          className='btn btn-ghost w-full '
+          className='btn btn-ghost w-full md:text-lg '
         >
           Start Recipe
         </button>
