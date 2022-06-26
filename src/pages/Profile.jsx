9e79acb96a810fd0cd4769b4c41965c18b36fb29
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-06-26 15:29:10 -0300

    feat: Create page Profile.

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import FooterMenu from '../components/FooterMenu';
+import Header from '../components/Header';
+
+export default function DoneRecipes() {
+  return (
+    <div className='flex flex-col min-h-full'>
+      <Header enableSearch={false} title={'Profile'} />
+      <div className="flex flex-col justify-center items-center p-2 bg-orange-100 grow">
+        <h2>Usuário</h2>
+        <button className="btn btn-primary btn-lg mx-2 transform transition duration-200 hover:scale-110">
+          Done Recipes
+        </button>
+        <button className="btn btn-primary btn-lg mx-2 transform transition duration-200 hover:scale-110">
+          Favorite Recipes
+        </button>
+        <button className="btn btn-primary btn-lg mx-2  transform transition duration-200 hover:scale-110">
+          Logout
+        </button>
+      </div>
+      <FooterMenu />
+    </div>
+  );
+}
